0dba1e494d1132cc602b0351c059fa09262e0176
Sks <124144700+sks123git@users.noreply.github.com>
2026-06-21 23:54:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let count = 0;
 
+function addItems(n) {
+    for (let i = 0; i < n; i++) {
+        count++;
+
+        const li = document.createElement("li");
+        li.textContent = `Item ${count}`;
+
+        list.appendChild(li);
+    }
+}
+
+addItems(10);
+
+list.addEventListener("scroll", () => {
+    if (
+        list.scrollTop + list.clientHeight >=
+        list.scrollHeight - 5
+    ) {
+        addItems(2);
+    }
+});
